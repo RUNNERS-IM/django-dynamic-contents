ba1a231d7215f6d8a8217255e4566b59261c854a
Sunwook KIm <sunus7@naver.com>
2024-01-10 03:39:24 +0900

    v0.1.12

--- dynamic_contents/admin.py
+++ dynamic_contents/admin.py
@@ -1,14 +1,36 @@
 # vim: set fileencoding=utf-8 :
+from django.conf import settings
 from django.contrib import admin
+
+# Third Party
+from modeltranslation.admin import TranslationAdmin
 
 # App
 from .models import Format, Part
 
 
 # Main Section
-class FormatAdmin(admin.ModelAdmin):
+class FormatAdmin(TranslationAdmin):
+    # 기본 list_display 설정
     list_display = ('id', 'type', 'subtype', 'content', 'created_at', 'updated_at')
-    search_fields = ('type', 'content')
+
+    def __init__(self, model, admin_site):
+        super().__init__(model, admin_site)
+        # modeltranslation으로 생성된 번역 필드를 가져와서 list_display에 추가
+        translated_fields = self.get_translated_fields()
+        self.list_display = self.list_display + translated_fields
+
+    def get_translated_fields(self):
+        # 모델의 모든 필드를 순회하면서 번역 필드 확인
+        translated_fields = []
+        for field in self.model._meta.get_fields():
+            if self.is_translated_field(field.name):
+                translated_fields.append(field.name)
+        return tuple(translated_fields)
+
+    def is_translated_field(self, field_name):
+        # 원래 필드명으로부터 생성된 번역 필드인지 확인
+        return '_' in field_name and field_name.split('_')[-1] in dict(settings.LANGUAGES).keys()
 
 
 class PartAdmin(admin.ModelAdmin):
