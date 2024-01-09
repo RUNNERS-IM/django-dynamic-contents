39f588fc265c79ceef8840f1dd3d07db6e83f7ac
Sunwook KIm <sunus7@naver.com>
2024-01-10 03:59:43 +0900

    v0.1.13

--- dynamic_contents/serializers.py
+++ dynamic_contents/serializers.py
@@ -5,13 +5,21 @@ from .models import Format, Part
 class FormatSerializer(serializers.ModelSerializer):
     class Meta:
         model = Format
-        fields = '__all__'
+        fields = ['id', 'type', 'subtype', 'content']
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        # modeltranslation에 의해 생성된 필드 추가
+        for field in ['content_en', 'content_ko', 'content_ja']:
+            if hasattr(instance, field):
+                representation[field] = getattr(instance, field)
+        return representation
 
 
 class PartSerializer(serializers.ModelSerializer):
     class Meta:
         model = Part
-        fields = '__all__'
+        fields = ['id', 'field', 'content', 'link', 'instance_id']
 
 
 class DynamicContentSerializerMixin:
@@ -19,4 +27,4 @@ class DynamicContentSerializerMixin:
     parts = PartSerializer(many=True, read_only=True)
 
     class Meta:
-        fields = ['id', 'format', 'parts', 'content_text', 'content_html', 'created_at', 'updated_at']
+        fields = ['id', 'format', 'parts', 'content_text', 'content_html']
